46582ab11e9f63a91ea4d3f6d6da7dee70566906
Adrian Samoticha <adrian@samoticha.de>
2022-07-01 13:30:29 +0200

    display blue line on sidebar when hovering

--- lib/widgets/sidebar.dart
+++ lib/widgets/sidebar.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_manual_widget_tester/util/mouse_cursor_overrider.dart';
 
@@ -15,26 +17,41 @@ class _HorizontalDragHandle extends StatefulWidget {
 class _HorizontalDragHandleState extends State<_HorizontalDragHandle> {
   int _mouseCursorOverrideId = 0;
   bool _isBeingDragged = false;
+  bool _isBeingHovered = false;
+  Timer? _hoverTimer;
   
   @override
   Widget build(BuildContext context) {
     return MouseRegion(
       cursor: SystemMouseCursors.resizeLeftRight,
-      child: GestureDetector(
-        onHorizontalDragStart: (details) {
-          _mouseCursorOverrideId = widget.mouseCursorOverrider.overrideMouseCursor(SystemMouseCursors.resizeLeftRight);
-          setState(() => _isBeingDragged = true);
-          widget.onDragStart();
+      child: MouseRegion(
+        onEnter: (event) => _hoverTimer = Timer(const Duration(milliseconds: 250), () {
+          setState(() {
+            _isBeingHovered = true;
+          });
+        }),
+        onExit: (event) {
+          _hoverTimer?.cancel();
+          setState(() {
+            _isBeingHovered = false;
+          });
         },
-        onHorizontalDragEnd: (details) {
-          widget.mouseCursorOverrider.cancelOverride(_mouseCursorOverrideId);
-          setState(() => _isBeingDragged = false);
-        },
-        onHorizontalDragUpdate: (details) => widget.onDragUpdate(details.delta.dx),
-        child: AnimatedContainer(
-          duration: const Duration(milliseconds: 150),
-          width: 6.0,
-          color: _isBeingDragged ? Colors.blue : Colors.transparent,
+        child: GestureDetector(
+          onHorizontalDragStart: (details) {
+            _mouseCursorOverrideId = widget.mouseCursorOverrider.overrideMouseCursor(SystemMouseCursors.resizeLeftRight);
+            setState(() => _isBeingDragged = true);
+            widget.onDragStart();
+          },
+          onHorizontalDragEnd: (details) {
+            widget.mouseCursorOverrider.cancelOverride(_mouseCursorOverrideId);
+            setState(() => _isBeingDragged = false);
+          },
+          onHorizontalDragUpdate: (details) => widget.onDragUpdate(details.delta.dx),
+          child: AnimatedContainer(
+            duration: const Duration(milliseconds: 150),
+            width: 6.0,
+            color: _isBeingDragged || _isBeingHovered ? Colors.blue : Colors.transparent,
+          ),
         ),
       ),
     );
