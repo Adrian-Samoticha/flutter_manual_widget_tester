a0608b86411ec9ad69ed8566c4747a3e917a06c6
Adrian Samoticha <adrian@samoticha.de>
2022-07-03 21:23:23 +0200

    add iconColor assert

--- lib/backend/widget_test_session.dart
+++ lib/backend/widget_test_session.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/widgets.dart';
 
 class WidgetTestSession {
-  WidgetTestSession({this.widget = const SizedBox(), this.name = 'unnamed', this.icon, this.iconColor}) : key = UniqueKey();
+  WidgetTestSession({this.widget = const SizedBox(), this.name = 'unnamed', this.icon, this.iconColor}) : key = UniqueKey(),
+    assert(iconColor == null || icon != null, 'iconColor must be null if icon is null.');
   
   final Key key;
   final Widget widget;
