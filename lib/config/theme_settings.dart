2ea55fb817f15a6c02cb303c77ba5bbeb3d1a9e2
Adrian Samoticha <adrian@samoticha.de>
2022-07-28 18:57:34 +0200

    implement radio button

--- lib/config/theme_settings.dart
+++ lib/config/theme_settings.dart
@@ -133,6 +133,11 @@ class ManualWidgetTesterThemeSettings {
   final Color doubleEditorInfiniteScrollViewIndicatorColor;
   final double doubleEditorInfiniteScrollViewIndicatorWidth;
   final double doubleEditorInfiniteScrollViewIndicatorHeight;
+  final Widget selectedRadioButton;
+  final Widget unselectedRadioButton;
+  final double spaceBetweenRadioButtonAndLabel;
+  final TextStyle selectedRadioButtonLabelStyle;
+  final TextStyle unselectedRadioButtonLabelStyle;
   
   const ManualWidgetTesterThemeSettings({
     this.backgroundColor = const Color.fromRGBO(41, 43, 53, 1.0),
@@ -377,5 +382,67 @@ class ManualWidgetTesterThemeSettings {
     this.doubleEditorInfiniteScrollViewIndicatorColor = const Color.fromRGBO(32, 145, 255, 1.0),
     this.doubleEditorInfiniteScrollViewIndicatorWidth = 2.31,
     this.doubleEditorInfiniteScrollViewIndicatorHeight = 0.75,
+    this.selectedRadioButton = const DecoratedBox(
+      decoration: BoxDecoration(
+        shape: BoxShape.circle,
+        color: Color.fromRGBO(64, 64, 64, 1.0),
+        border: Border.fromBorderSide(
+          BorderSide(
+            color: Color.fromRGBO(150, 150, 150, 1.0),
+          ),
+        ),
+        boxShadow: [
+          BoxShadow(color: Color.fromRGBO(0, 0, 0, 0.5), blurRadius: 2.0, offset: Offset(0.5, 1.0), spreadRadius: 1.0),
+        ],
+      ),
+      child: FractionallySizedBox(
+        widthFactor: 0.67,
+        heightFactor: 0.67,
+        child: DecoratedBox(
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            boxShadow: [
+              BoxShadow(color: Color.fromRGBO(24, 125, 192, 1.0)),
+              BoxShadow(color: Color.fromRGBO(32, 175, 255, 1.0), spreadRadius: -2.0, blurRadius: 2.5, offset: Offset(-1.0, -1.0)),
+            ],
+          ),
+          child: FractionallySizedBox(
+            widthFactor: 0.24,
+            heightFactor: 0.24,
+            alignment: Alignment(-0.35, -0.35),
+            child: DecoratedBox(
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                gradient: LinearGradient(
+                  colors: [
+                    Color.fromRGBO(255, 255, 255, 0.3),
+                    Color.fromRGBO(255, 255, 255, 0.25),
+                    Color.fromRGBO(255, 255, 255, 0.05),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    ),
+    this.unselectedRadioButton = const DecoratedBox(
+      decoration: BoxDecoration(
+        shape: BoxShape.circle,
+        color: Color.fromRGBO(52, 52, 52, 1.0),
+        border: Border.fromBorderSide(
+          BorderSide(
+            color: Color.fromRGBO(82, 82, 82, 1.0),
+          ),
+        ),
+      ),
+    ),
+    this.spaceBetweenRadioButtonAndLabel = 12.0,
+    this.selectedRadioButtonLabelStyle = const TextStyle(
+      color: Colors.white,
+    ),
+    this.unselectedRadioButtonLabelStyle = const TextStyle(
+      color: Color.fromRGBO(255, 255, 255, 0.5),
+    ),
   });
 }
---
+++ lib/widgets/ui_elements/radio_button.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_manual_widget_tester/config/theme_settings.dart';
+
+class ManualWidgetTesterRadioButton extends StatelessWidget {
+  final ManualWidgetTesterThemeSettings themeSettings;
+  final bool isSelected;
+  
+  const ManualWidgetTesterRadioButton({Key? key, required this.isSelected, required this.themeSettings}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    if (isSelected) {
+      return themeSettings.selectedRadioButton;
+    }
+    
+    return themeSettings.unselectedRadioButton;
+  }
+}
+
+class ManualWidgetTesterRadioButtonWithLabel extends StatelessWidget {
+  final ManualWidgetTesterThemeSettings themeSettings;
+  final bool isSelected;
+  final String label;
+  
+  const ManualWidgetTesterRadioButtonWithLabel({Key? key, required this.themeSettings, required this.isSelected, required this.label}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Center(
+          child: AspectRatio(
+            aspectRatio: 1.0,
+            child: ManualWidgetTesterRadioButton(
+              themeSettings: themeSettings,
+              isSelected: isSelected,
+            ),
+          ),
+        ),
+        SizedBox(width: themeSettings.spaceBetweenRadioButtonAndLabel),
+        Expanded(
+          child: Text(
+            label,
+            softWrap: false,
+            overflow: TextOverflow.fade,
+            style: isSelected ? themeSettings.selectedRadioButtonLabelStyle : themeSettings.unselectedRadioButtonLabelStyle 
+          ),
+        ),
+      ],
+    );
+  }
+}
