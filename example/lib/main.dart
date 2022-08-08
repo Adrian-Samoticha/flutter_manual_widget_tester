64eb0b403ac61e60f2689b98d75ef3acf48942ce
Adrian Samoticha <adrian@samoticha.de>
2022-08-08 14:06:11 +0200

    add mock builders to main.dart for testing purposes

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -31,7 +31,7 @@ class _MyHomePageState extends State<MyHomePage> {
     final builders = [
       WidgetTestBuilder(
         key: const ValueKey('some key'),
-        name: 'some name',
+        name: 'SomeName',
         icon: Icons.access_alarms_rounded,
         builder: (context, settings) {
           final backgroundColor = settings.getSetting('backgroundColor', Colors.white);
@@ -43,13 +43,74 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       WidgetTestBuilder(
         key: const ValueKey('some other key'),
-        name: 'some other name',
+        name: 'SomeOtherName',
         icon: Icons.kayaking,
         builder: (context, settings) {
           final backgroundColor = settings.getSetting('backgroundColor', Colors.orange);
           return Container(
             color: backgroundColor,
             child: const Text('foobar6'),
+          );
+        },
+      ),
+      WidgetTestBuilder(
+        key: const ValueKey('list view'),
+        name: 'ListView',
+        icon: Icons.list,
+        builder: (context, settings) {
+          final backgroundColor = settings.getSetting('backgroundColor', Colors.white);
+          return Container(
+            color: backgroundColor,
+            child: const Text('list view'),
+          );
+        },
+      ),
+      WidgetTestBuilder(
+        key: const ValueKey('dialog generator'),
+        name: 'DialogGenerator',
+        icon: Icons.window,
+        builder: (context, settings) {
+          final backgroundColor = settings.getSetting('backgroundColor', Colors.blue);
+          return Container(
+            color: backgroundColor,
+            child: const Text('dialog generator'),
+          );
+        },
+      ),
+      WidgetTestBuilder(
+        key: const ValueKey('string editor state'),
+        name: 'StringEditorState',
+        icon: Icons.abc,
+        builder: (context, settings) {
+          final backgroundColor = settings.getSetting('backgroundColor', Colors.lightGreen);
+          return Container(
+            color: backgroundColor,
+            child: const Text('edit this string\'s state'),
+          );
+        },
+      ),
+      WidgetTestBuilder(
+        key: const ValueKey('substring editor'),
+        name: 'SubstringEditor',
+        icon: Icons.abc,
+        iconColor: Colors.red,
+        builder: (context, settings) {
+          final backgroundColor = settings.getSetting('backgroundColor', Colors.green);
+          return Container(
+            color: backgroundColor,
+            child: const Text('it this str'),
+          );
+        },
+      ),
+      WidgetTestBuilder(
+        key: const ValueKey('string editor'),
+        name: 'StringEditor',
+        icon: Icons.abc,
+        builder: (context, settings) {
+          final backgroundColor = settings.getSetting('backgroundColor', Colors.green);
+          return Container(
+            color: backgroundColor,
+            child: const Text('edit this string'),
           );
         },
       ),
