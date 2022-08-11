55842d96f97e3dd5d7127783ef7cb17bae4b496d
Adrian Samoticha <adrian@samoticha.de>
2022-08-11 12:38:54 +0200

    add exports

--- lib/flutter_manual_widget_tester.dart
+++ lib/flutter_manual_widget_tester.dart
@@ -20,6 +20,9 @@ import 'package:flutter_manual_widget_tester/widgets/custom_settings_editors/edi
 import 'package:flutter_manual_widget_tester/widgets/sidebar/sidebar.dart';
 import 'package:flutter_manual_widget_tester/widgets/widget_test_session_area_stack/widget_test_session_area_stack.dart';
 
+export 'package:flutter_manual_widget_tester/config/theme_settings.dart';
+export 'package:flutter_manual_widget_tester/backend/widget_test_session_handler/widget_test_builder.dart';
+
 class ManualWidgetTester extends StatefulWidget {
   const ManualWidgetTester({Key? key, this.themeSettings = const ManualWidgetTesterThemeSettings(), this.doubleEditorInfiniteScrollViewRange = 3.0, this.doubleEditorInfiniteScrollViewScrollSpeedFactor = 0.003, required this.builders}) : super(key: key);
   
