eecd001b5b02bd24b8176a71d3786ca22d1d97ec
Adrian Samoticha <adrian@samoticha.de>
2022-11-17 12:36:28 +0100

    disable app bar shadow when no tab is open

--- lib/widgets/app_bar/app_bar.dart
+++ lib/widgets/app_bar/app_bar.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_manual_widget_tester/backend/widget_test_session_handler/widget_test_builder.dart';
 import 'package:flutter_manual_widget_tester/backend/widget_test_session_handler/widget_test_session_handler.dart';
@@ -7,7 +9,7 @@ import 'app_bar_shadow.dart';
 import 'new_test_session_button.dart';
 import 'tab_bar/tab_bar.dart';
 
-class ManualWidgetTesterAppBar extends StatelessWidget {
+class ManualWidgetTesterAppBar extends StatefulWidget {
   const ManualWidgetTesterAppBar(
       {Key? key,
       required this.themeSettings,
@@ -20,28 +22,69 @@ class ManualWidgetTesterAppBar extends StatelessWidget {
   final List<WidgetTestBuilder> builders;
 
   @override
+  State<ManualWidgetTesterAppBar> createState() =>
+      _ManualWidgetTesterAppBarState();
+}
+
+class _ManualWidgetTesterAppBarState extends State<ManualWidgetTesterAppBar> {
+  late double _appBarShadowOpacity;
+  late StreamSubscription<WidgetTestSessionHandler>
+      _widgetTestSessionHandlerOnChangedCallbackStreamSubscription;
+
+  @override
+  void initState() {
+    _appBarShadowOpacity =
+        widget.widgetTestSessionHandler.widgetTestSessions.isEmpty ? 0.0 : 1.0;
+
+    _widgetTestSessionHandlerOnChangedCallbackStreamSubscription =
+        widget.widgetTestSessionHandler.registerOnChangedCallback(
+      (WidgetTestSessionHandler sessionHandler) {
+        setState(
+          () {
+            _appBarShadowOpacity =
+                sessionHandler.widgetTestSessions.isEmpty ? 0.0 : 1.0;
+          },
+        );
+      },
+    );
+
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _widgetTestSessionHandlerOnChangedCallbackStreamSubscription.cancel();
+
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Container(
-      height: themeSettings.appBarHeight,
-      color: themeSettings.sidebarColor,
+      height: widget.themeSettings.appBarHeight,
+      color: widget.themeSettings.sidebarColor,
       child: Stack(
         children: [
-          AppBarShadow(themeSettings: themeSettings),
+          AnimatedOpacity(
+            duration: widget.themeSettings.tabOpenAnimationDuration,
+            opacity: _appBarShadowOpacity,
+            child: AppBarShadow(themeSettings: widget.themeSettings),
+          ),
           Row(
             children: [
               Expanded(
                 child: ManualWidgetTesterTabBar(
-                  themeSettings: themeSettings,
-                  widgetTestSessionHandler: widgetTestSessionHandler,
+                  themeSettings: widget.themeSettings,
+                  widgetTestSessionHandler: widget.widgetTestSessionHandler,
                 ),
               ),
               Container(
-                width: themeSettings.appBarHeight,
-                padding: themeSettings.createTestSessionButtonPadding,
+                width: widget.themeSettings.appBarHeight,
+                padding: widget.themeSettings.createTestSessionButtonPadding,
                 child: NewTestSessionButton(
-                  themeSettings: themeSettings,
-                  builders: builders,
-                  widgetTestSessionHandler: widgetTestSessionHandler,
+                  themeSettings: widget.themeSettings,
+                  builders: widget.builders,
+                  widgetTestSessionHandler: widget.widgetTestSessionHandler,
                 ),
               ),
             ],
@@ -51,4 +94,3 @@ class ManualWidgetTesterAppBar extends StatelessWidget {
     );
   }
 }
-
