a5cb24ac20b90d07d793f53636184027d0efd107
Adrian Samoticha <adrian@samoticha.de>
2022-12-08 09:46:43 +0100

    document clamped double

--- lib/backend/constrained_types/clamped_double.dart
+++ lib/backend/constrained_types/clamped_double.dart
@@ -9,12 +9,38 @@ class ClampedDouble {
     _value = value.clamp(lowerLimit, upperLimit);
   }
 
+  /// Creates a [ClampedDouble] whose [value] is guaranteed to be clamped to be
+  /// in the range [lowerLimit]-[upperLimit].
+  ///
+  /// It is intended to be used in conjunction with
+  /// [WidgetTestSessionCustomSettings.getSetting] to create a custom setting
+  /// whose value is a clamped double.
+  ///
+  /// Usage example:
+  /// ```dart
+  /// WidgetTestBuilder(
+  ///   id: 'my widget',
+  ///   name: 'MyWidget',
+  ///   builder: (context, settings) {
+  ///     final someDouble = settings
+  ///         .getSetting('someDouble',
+  ///             ClampedDouble(lowerLimit: 0.5, value: 0.5, upperLimit: 1.5))
+  ///         .value;
+  ///
+  ///     return MyWidget(
+  ///       someDouble: someDouble,
+  ///     );
+  ///   },
+  /// )
+  /// ```
   ClampedDouble(
       {this.lowerLimit = double.negativeInfinity,
       this.upperLimit = double.infinity,
       required value}) {
-    assert(upperLimit >= lowerLimit,
-        'upperLimit ($upperLimit) must be greater than or equal to lowerLimit ($lowerLimit).');
+    assert(
+        upperLimit >= lowerLimit,
+        'upperLimit ($upperLimit) must be greater than or equal to lowerLimit'
+        '($lowerLimit).');
 
     this.value = value;
   }
