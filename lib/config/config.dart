c16accb18ce552e75164b1f694bd0532492b85f4
Adrian Samoticha <adrian@samoticha.de>
2022-12-18 16:28:14 +0100

    document config class

--- lib/config/config.dart
+++ lib/config/config.dart
@@ -1,8 +1,18 @@
 class ManualWidgetTesterConfig {
-  final double doubleEditorInfiniteScrollViewRange;
-  final double doubleEditorInfiniteScrollViewScrollSpeedFactor;
-
+  /// Creates a configuration object for the [ManualWidgetTester].
+  ///
+  /// The [doubleEditorInfiniteScrollViewRange] is the range of the double
+  /// editor infinite scroll view.
+  ///
+  /// The [doubleEditorInfiniteScrollViewScrollSpeedFactor] is the scroll speed
+  /// factor of the double editor infinite scroll view.
   ManualWidgetTesterConfig(
       {required this.doubleEditorInfiniteScrollViewRange,
       required this.doubleEditorInfiniteScrollViewScrollSpeedFactor});
+
+  /// The range of the double editor infinite scroll view.
+  final double doubleEditorInfiniteScrollViewRange;
+
+  /// The scroll speed factor of the double editor infinite scroll view.
+  final double doubleEditorInfiniteScrollViewScrollSpeedFactor;
 }
