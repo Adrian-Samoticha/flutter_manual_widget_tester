c32650cbd22dafb62ad362006443c967829f9fc7
Adrian Samoticha <adrian@samoticha.de>
2023-01-08 17:49:03 +0100

    let custom settings theme extend equatable

--- lib/config/theme_config/custom_settings_theme.dart
+++ lib/config/theme_config/custom_settings_theme.dart
@@ -1,10 +1,16 @@
+import 'package:equatable/equatable.dart';
 import 'package:flutter/material.dart';
 
-class ManualWidgetTesterCustomSettingsTheme {
+class ManualWidgetTesterCustomSettingsTheme extends Equatable {
   /// The padding of a custom settings widget.
   final EdgeInsets customSettingsPadding;
 
   const ManualWidgetTesterCustomSettingsTheme({
     this.customSettingsPadding = const EdgeInsets.all(8.0),
   });
+
+  @override
+  List<Object?> get props => [
+        customSettingsPadding,
+      ];
 }
