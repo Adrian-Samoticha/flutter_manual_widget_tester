90c4d27e6c0a9cfab46719001a6ac7f6f86fc120
Adrian Samoticha <adrian@samoticha.de>
2023-01-08 17:47:21 +0100

    let string editor theme extend equatable

--- lib/config/theme_config/string_editor_theme.dart
+++ lib/config/theme_config/string_editor_theme.dart
@@ -1,8 +1,15 @@
-class ManualWidgetTesterStringEditorTheme {
+import 'package:equatable/equatable.dart';
+
+class ManualWidgetTesterStringEditorTheme extends Equatable {
   /// The height of the string editor.
   final double stringEditorHeight;
 
   const ManualWidgetTesterStringEditorTheme({
     this.stringEditorHeight = 32.0,
   });
+
+  @override
+  List<Object?> get props => [
+        stringEditorHeight,
+      ];
 }
