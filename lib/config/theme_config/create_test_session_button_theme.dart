046a465b83a149b31fc06ebe6b74c283d3a5a5ce
Adrian Samoticha <adrian@samoticha.de>
2023-01-08 17:47:56 +0100

    let “create test session” button theme extend equatable

--- lib/config/theme_config/create_test_session_button_theme.dart
+++ lib/config/theme_config/create_test_session_button_theme.dart
@@ -1,10 +1,16 @@
+import 'package:equatable/equatable.dart';
 import 'package:flutter/material.dart';
 
-class ManualWidgetTesterCreateTestSessionButton {
+class ManualWidgetTesterCreateTestSessionButton extends Equatable {
   /// The padding of the “create test session” button.
   final EdgeInsets createTestSessionButtonPadding;
 
   const ManualWidgetTesterCreateTestSessionButton({
     this.createTestSessionButtonPadding = const EdgeInsets.all(6.5),
   });
+
+  @override
+  List<Object?> get props => [
+        createTestSessionButtonPadding,
+      ];
 }
