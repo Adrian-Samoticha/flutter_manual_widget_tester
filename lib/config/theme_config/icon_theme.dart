1e3c5b89e67c55733f8773a6da726e5afe982815
Adrian Samoticha <adrian@samoticha.de>
2023-01-08 17:48:29 +0100

    let icon theme extend equatable

--- lib/config/theme_config/icon_theme.dart
+++ lib/config/theme_config/icon_theme.dart
@@ -1,10 +1,16 @@
+import 'package:equatable/equatable.dart';
 import 'package:flutter/material.dart';
 
-class ManualWidgetTesterIconTheme {
+class ManualWidgetTesterIconTheme extends Equatable {
   /// The default color of the icons of the open test sessions.
   final Color defaultIconColor;
 
   const ManualWidgetTesterIconTheme({
     this.defaultIconColor = const Color.fromRGBO(64, 167, 255, 1.0),
   });
+
+  @override
+  List<Object?> get props => [
+        defaultIconColor,
+      ];
 }
