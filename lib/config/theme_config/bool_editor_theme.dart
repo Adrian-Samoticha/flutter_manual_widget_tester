e09a9cee875d848af90644885a53d26352cf8641
Adrian Samoticha <adrian@samoticha.de>
2023-01-21 20:10:00 +0100

    add `fromThemeGeneratorParameters` to bool editor theme

--- lib/config/theme_config/bool_editor_theme.dart
+++ lib/config/theme_config/bool_editor_theme.dart
@@ -1,6 +1,8 @@
 import 'package:equatable/equatable.dart';
 import 'package:flutter/material.dart';
 
+import 'theme_generator/theme_generator_parameters.dart';
+
 class ManualWidgetTesterBoolEditorTheme extends Equatable {
   /// The maximal width of a boolean editor.
   final double boolEditorMaxWidth;
@@ -29,4 +31,60 @@ class ManualWidgetTesterBoolEditorTheme extends Equatable {
         boolEditorRadioButtonPadding,
         boolEditorHeight,
       ];
+
+  static ManualWidgetTesterBoolEditorTheme fromThemeGeneratorParameters(
+      ThemeGeneratorParameters parameters) {
+    return ManualWidgetTesterBoolEditorTheme(
+      boolEditorMaxWidth: _getBoolEditorMaxWidthFromLayout(parameters.layout),
+      boolEditorSpaceBetweenRadioButtons:
+          _getBoolEditorSpaceBetweenRadioButtons(parameters.layout),
+      boolEditorRadioButtonPadding:
+          _getBoolEditorRadioButtonPadding(parameters.layout),
+      boolEditorHeight: _getBoolEditorHeightFromLayout(parameters.layout),
+    );
+  }
+
+  static double _getBoolEditorMaxWidthFromLayout(Layout layout) {
+    switch (layout) {
+      case Layout.compact:
+        return 160.0;
+      case Layout.normal:
+        return 192.0;
+      case Layout.cozy:
+        return 200.0;
+    }
+  }
+
+  static double _getBoolEditorSpaceBetweenRadioButtons(Layout layout) {
+    switch (layout) {
+      case Layout.compact:
+        return 8.0;
+      case Layout.normal:
+        return 12.0;
+      case Layout.cozy:
+        return 16.0;
+    }
+  }
+
+  static EdgeInsets _getBoolEditorRadioButtonPadding(Layout layout) {
+    switch (layout) {
+      case Layout.compact:
+        return const EdgeInsets.symmetric(vertical: 6.0);
+      case Layout.normal:
+        return const EdgeInsets.symmetric(vertical: 8.0);
+      case Layout.cozy:
+        return const EdgeInsets.symmetric(vertical: 10.0);
+    }
+  }
+
+  static double _getBoolEditorHeightFromLayout(Layout layout) {
+    switch (layout) {
+      case Layout.compact:
+        return 12.0;
+      case Layout.normal:
+        return 16.0;
+      case Layout.cozy:
+        return 20.0;
+    }
+  }
 }
