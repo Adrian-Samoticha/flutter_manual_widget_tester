e6fa2488cd191af163652dbcb669a66ea32e8240
Adrian Samoticha <adrian@samoticha.de>
2023-01-21 20:33:59 +0100

    add `fromThemeGeneratorParameters` to button theme

--- lib/config/theme_config/button_theme.dart
+++ lib/config/theme_config/button_theme.dart
@@ -1,6 +1,9 @@
 import 'package:equatable/equatable.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_color/flutter_color.dart';
 import 'package:gradient_borders/gradient_borders.dart';
+
+import 'theme_generator/theme_generator_parameters.dart';
 
 class ManualWidgetTesterButtonTheme extends Equatable {
   /// The buttons' color.
@@ -116,4 +119,93 @@ class ManualWidgetTesterButtonTheme extends Equatable {
         buttonTextStyle,
         buttonBoxBorder,
       ];
+
+  static ManualWidgetTesterButtonTheme fromThemeGeneratorParameters(
+      ThemeGeneratorParameters parameters) {
+    return ManualWidgetTesterButtonTheme(
+      buttonColor: parameters.backgroundColor.lighter(32),
+      buttonShadow:
+          _getButtonShadowFromDesignLanguage(parameters.designLanguage),
+      disabledButtonIconTheme: IconThemeData(
+        color: _getColorFromBrightness(parameters.brightness).withOpacity(0.25),
+        size: _getIconSizeFromLayout(parameters.layout),
+      ),
+      pressedButtonIconTheme: IconThemeData(
+        color: _getColorFromBrightness(parameters.brightness).withOpacity(0.5),
+        size: _getIconSizeFromLayout(parameters.layout),
+      ),
+      buttonIconTheme: IconThemeData(
+        color: _getColorFromBrightness(parameters.brightness).withOpacity(0.9),
+        size: _getIconSizeFromLayout(parameters.layout),
+      ),
+      disabledButtonTextStyle: TextStyle(
+        color: _getColorFromBrightness(parameters.brightness).withOpacity(0.25),
+      ),
+      pressedButtonTextStyle: TextStyle(
+        color: _getColorFromBrightness(parameters.brightness).withOpacity(0.5),
+      ),
+      buttonTextStyle: TextStyle(
+        color: _getColorFromBrightness(parameters.brightness).withOpacity(0.9),
+      ),
+      buttonBoxBorder:
+          _getButtonBoxBorderFromDesignLanguage(parameters.designLanguage),
+    );
+  }
+
+  static List<BoxShadow> _getButtonShadowFromDesignLanguage(
+      DesignLanguage designLanguage) {
+    switch (designLanguage) {
+      case DesignLanguage.skeuomorphic:
+        return const [
+          BoxShadow(
+            blurRadius: 2.0,
+            color: Color.fromRGBO(0, 0, 0, 0.2),
+            offset: Offset(0.0, 1.0),
+          ),
+        ];
+      case DesignLanguage.flat:
+        return const [];
+    }
+  }
+
+  static Color _getColorFromBrightness(Brightness brightness) {
+    switch (brightness) {
+      case Brightness.dark:
+        return const Color.fromRGBO(255, 255, 255, 1.0);
+      case Brightness.light:
+        return const Color.fromRGBO(0, 0, 0, 1.0);
+    }
+  }
+
+  static double _getIconSizeFromLayout(Layout layout) {
+    switch (layout) {
+      case Layout.compact:
+        return 16.0;
+      case Layout.normal:
+        return 18.5;
+      case Layout.cozy:
+        return 20.0;
+    }
+  }
+
+  static BoxBorder _getButtonBoxBorderFromDesignLanguage(
+      DesignLanguage designLanguage) {
+    switch (designLanguage) {
+      case DesignLanguage.skeuomorphic:
+        return const GradientBoxBorder(
+          gradient: LinearGradient(
+            colors: [
+              Color.fromRGBO(255, 255, 255, 0.1),
+              Colors.transparent,
+              Colors.transparent,
+            ],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+          ),
+          width: 1.0,
+        );
+      case DesignLanguage.flat:
+        return const Border();
+    }
+  }
 }
