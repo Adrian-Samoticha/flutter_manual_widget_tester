dbb2ed15dec5f0a1831022c9e01ce29268edb801
Adrian Samoticha <adrian@samoticha.de>
2023-01-21 20:34:05 +0100

    add `fromThemeGeneratorParameters` to close button theme

--- lib/config/theme_config/close_button_theme.dart
+++ lib/config/theme_config/close_button_theme.dart
@@ -1,6 +1,8 @@
 import 'package:equatable/equatable.dart';
 import 'package:flutter/material.dart';
 
+import 'theme_generator/theme_generator_parameters.dart';
+
 class ManualWidgetTesterCloseButtonTheme extends Equatable {
   /// The box decoration of a close button's hover effect.
   final BoxDecoration closeButtonHoverDecoration;
@@ -21,4 +23,21 @@ class ManualWidgetTesterCloseButtonTheme extends Equatable {
         closeButtonHoverDecoration,
         closeButtonColor,
       ];
+
+  static ManualWidgetTesterCloseButtonTheme fromThemeGeneratorParameters(
+      ThemeGeneratorParameters parameters) {
+    return ManualWidgetTesterCloseButtonTheme(
+      closeButtonColor:
+          _getCloseButtonColorFromBrightness(parameters.brightness),
+    );
+  }
+
+  static Color _getCloseButtonColorFromBrightness(Brightness brightness) {
+    switch (brightness) {
+      case Brightness.dark:
+        return const Color.fromRGBO(255, 255, 255, 0.9);
+      case Brightness.light:
+        return const Color.fromRGBO(0, 0, 0, 0.9);
+    }
+  }
 }
