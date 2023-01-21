9f49bb2e3d168ddff8059f934475f2d277758bcf
Adrian Samoticha <adrian@samoticha.de>
2023-01-21 21:33:22 +0100

    add `fromThemeGeneratorParameters` to create test session dialog theme

--- lib/config/theme_config/create_test_session_dialog_theme.dart
+++ lib/config/theme_config/create_test_session_dialog_theme.dart
@@ -1,6 +1,8 @@
 import 'package:equatable/equatable.dart';
 import 'package:flutter/material.dart';
 
+import 'theme_generator/theme_generator_parameters.dart';
+
 class ManualWidgetTesterCreateTestSessionDialogTheme extends Equatable {
   /// The width of the “create test session” button.
   final double createTestSessionDialogWidth;
@@ -107,4 +109,172 @@ class ManualWidgetTesterCreateTestSessionDialogTheme extends Equatable {
         createTestSessionDialogUnselectedSearchResultOpacity,
         createTestSessionDialogNoMatchingResultsTextStyle,
       ];
+
+  static ManualWidgetTesterCreateTestSessionDialogTheme
+      fromThemeGeneratorParameters(ThemeGeneratorParameters parameters) {
+    return ManualWidgetTesterCreateTestSessionDialogTheme(
+      createTestSessionDialogPadding:
+          _getCreateTestSessionDialogPaddingFromLayout(parameters.layout),
+      createTestSessionDialogSearchBarHeight:
+          _getCreateTestSessionDialogSearchBarHeightFromLayout(
+              parameters.layout),
+      createTestSessionDialogSearchResultsPadding:
+          _getCreateTestSessionDialogSearchResultsPaddingFromLayout(
+              parameters.layout),
+      createTestSessionDialogSearchResultHeight:
+          _getCreateTestSessionDialogSearchResultHeightFromLayout(
+              parameters.layout),
+      createTestSessionDialogSelectedSearchResultDecoration:
+          _getCreateTestSessionDialogSelectedSearchResultDecorationFromBrightness(
+              parameters.brightness),
+      createTestSessionDialogSearchResultTextStyle:
+          _getCreateTestSessionDialogSearchResultTextStyleFromBrightness(
+              parameters.brightness),
+      createTestSessionDialogSearchResultIconSize:
+          _getCreateTestSessionDialogSearchResultIconSizeFromLayout(
+              parameters.layout),
+      createTestSessionDialogSearchResultFadeDuration:
+          _getCreateTestSessionDialogSearchResultFadeDurationFromAnimationSpeed(
+              parameters.animationSpeed),
+      createTestSessionDialogNoMatchingResultsTextStyle:
+          _getCreateTestSessionDialogNoMatchingResultsTextStyleFromBrightness(
+              parameters.brightness),
+    );
+  }
+
+  static EdgeInsets _getCreateTestSessionDialogPaddingFromLayout(
+      Layout layout) {
+    switch (layout) {
+      case Layout.compact:
+        return const EdgeInsets.all(5.5);
+      case Layout.normal:
+        return const EdgeInsets.all(8.0);
+      case Layout.cozy:
+        return const EdgeInsets.all(11.5);
+    }
+  }
+
+  static double _getCreateTestSessionDialogSearchBarHeightFromLayout(
+      Layout layout) {
+    switch (layout) {
+      case Layout.compact:
+        return 24.0;
+      case Layout.normal:
+        return 32.0;
+      case Layout.cozy:
+        return 48.0;
+    }
+  }
+
+  static EdgeInsets _getCreateTestSessionDialogSearchResultsPaddingFromLayout(
+      Layout layout) {
+    switch (layout) {
+      case Layout.compact:
+        return const EdgeInsets.only(top: 5.5);
+      case Layout.normal:
+        return const EdgeInsets.only(top: 8.0);
+      case Layout.cozy:
+        return const EdgeInsets.only(top: 11.5);
+    }
+  }
+
+  static double _getCreateTestSessionDialogSearchResultHeightFromLayout(
+      Layout layout) {
+    switch (layout) {
+      case Layout.compact:
+        return 24.0;
+      case Layout.normal:
+        return 28.0;
+      case Layout.cozy:
+        return 32.0;
+    }
+  }
+
+  static BoxDecoration
+      _getCreateTestSessionDialogSelectedSearchResultDecorationFromBrightness(
+          Brightness brightness) {
+    switch (brightness) {
+      case Brightness.dark:
+        return const BoxDecoration(
+          color: Color.fromRGBO(255, 255, 255, 0.15),
+          border: Border.fromBorderSide(
+            BorderSide(
+              color: Color.fromRGBO(255, 255, 255, 0.15),
+            ),
+          ),
+          borderRadius: BorderRadius.all(
+            Radius.circular(5.0),
+          ),
+        );
+      case Brightness.light:
+        return const BoxDecoration(
+          color: Color.fromRGBO(0, 0, 0, 0.15),
+          border: Border.fromBorderSide(
+            BorderSide(
+              color: Color.fromRGBO(0, 0, 0, 0.15),
+            ),
+          ),
+          borderRadius: BorderRadius.all(
+            Radius.circular(5.0),
+          ),
+        );
+    }
+  }
+
+  static TextStyle
+      _getCreateTestSessionDialogSearchResultTextStyleFromBrightness(
+          Brightness brightness) {
+    switch (brightness) {
+      case Brightness.dark:
+        return const TextStyle(
+          color: Colors.white,
+        );
+      case Brightness.light:
+        return const TextStyle(
+          color: Colors.black,
+        );
+    }
+  }
+
+  static double _getCreateTestSessionDialogSearchResultIconSizeFromLayout(
+      Layout layout) {
+    switch (layout) {
+      case Layout.compact:
+        return 18.0;
+      case Layout.normal:
+        return 21.0;
+      case Layout.cozy:
+        return 24.0;
+    }
+  }
+
+  static Duration
+      _getCreateTestSessionDialogSearchResultFadeDurationFromAnimationSpeed(
+          AnimationSpeed animationSpeed) {
+    switch (animationSpeed) {
+      case AnimationSpeed.instant:
+        return const Duration();
+      case AnimationSpeed.quick:
+        return const Duration(milliseconds: 50);
+      case AnimationSpeed.normal:
+        return const Duration(milliseconds: 100);
+      case AnimationSpeed.slow:
+        return const Duration(milliseconds: 200);
+    }
+  }
+
+  static TextStyle
+      _getCreateTestSessionDialogNoMatchingResultsTextStyleFromBrightness(
+          Brightness brightness) {
+    switch (brightness) {
+      case Brightness.dark:
+        return const TextStyle(
+          color: Color.fromRGBO(255, 255, 255, 0.9),
+        );
+      case Brightness.light:
+        return const TextStyle(
+          color: Color.fromRGBO(0, 0, 0, 0.9),
+        );
+    }
+  }
 }
