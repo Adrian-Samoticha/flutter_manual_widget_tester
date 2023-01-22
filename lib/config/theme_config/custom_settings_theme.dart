2cb092c3ca6da766f460161816dde4c9a20d5353
Adrian Samoticha <adrian@samoticha.de>
2023-01-22 10:40:09 +0100

    add `fromThemeGeneratorParameters` to custom settings theme

--- lib/config/theme_config/custom_settings_theme.dart
+++ lib/config/theme_config/custom_settings_theme.dart
@@ -1,6 +1,8 @@
 import 'package:equatable/equatable.dart';
 import 'package:flutter/material.dart';
 
+import 'theme_generator/theme_generator_parameters.dart';
+
 class ManualWidgetTesterCustomSettingsTheme extends Equatable {
   /// The padding of a custom settings widget.
   final EdgeInsets customSettingsPadding;
@@ -13,4 +15,23 @@ class ManualWidgetTesterCustomSettingsTheme extends Equatable {
   List<Object?> get props => [
         customSettingsPadding,
       ];
+
+  static ManualWidgetTesterCustomSettingsTheme fromThemeGeneratorParameters(
+      ThemeGeneratorParameters parameters) {
+    return ManualWidgetTesterCustomSettingsTheme(
+      customSettingsPadding:
+          _getCustomSettingsPaddingFromLayout(parameters.layout),
+    );
+  }
+
+  static EdgeInsets _getCustomSettingsPaddingFromLayout(Layout layout) {
+    switch (layout) {
+      case Layout.compact:
+        return const EdgeInsets.all(6.0);
+      case Layout.normal:
+        return const EdgeInsets.all(8.0);
+      case Layout.cozy:
+        return const EdgeInsets.all(12.0);
+    }
+  }
 }
