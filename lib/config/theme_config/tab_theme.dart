1dd5cbf5cc38e5df9b38670ae89c8bb01615a102
Adrian Samoticha <adrian@samoticha.de>
2023-01-22 13:26:39 +0100

    implement `fromThemeGeneratorParameters` for tab theme

--- lib/config/theme_config/tab_theme.dart
+++ lib/config/theme_config/tab_theme.dart
@@ -143,6 +143,7 @@ class ManualWidgetTesterTabTheme extends Equatable {
 
   @override
   List<Object?> get props => [
+        minTabWidth,
         maxTabWidth,
         focusedTabBoxDecoration,
         unfocusedTabBoxDecoration,
@@ -163,6 +164,181 @@ class ManualWidgetTesterTabTheme extends Equatable {
 
   static ManualWidgetTesterTabTheme fromThemeGeneratorParameters(
       ThemeGeneratorParameters parameters) {
-    return const ManualWidgetTesterTabTheme();
+    return ManualWidgetTesterTabTheme(
+      minTabWidth: _getMinTabWidthFromLayout(parameters.layout),
+      maxTabWidth: _getMaxTabWidthFromLayout(parameters.layout),
+      focusedTabBoxDecoration: _getFocusedTabBoxDecoration(parameters),
+      // unfocusedTabBoxDecoration:
+      // focusedTabAccentColorDecorationWidth:
+      tabSeparatorBoxDecoration:
+          _getTabSeparatorBoxDecorationFromBrightness(parameters.brightness),
+      tabLightReflectionBoxDecoration:
+          _getTabLightReflectionBoxDecorationFromDesignLanguage(
+              parameters.designLanguage),
+      tabTextStyle: _getTabTextStyle(parameters),
+      unfocusedTabOpacityChangeDuration:
+          _getUnfocusedTabOpacityChangeDurationFromAnimationSpeed(
+              parameters.animationSpeed),
+      tabContentPadding: _getTabContentPaddingFromLayout(parameters.layout),
+      tabOpenAnimationDuration: _getTabOpenAnimationDurationFromAnimationSpeed(
+          parameters.animationSpeed),
+    );
+  }
+
+  static double _getMinTabWidthFromLayout(Layout layout) {
+    switch (layout) {
+      case Layout.compact:
+        return 62.0;
+      case Layout.normal:
+        return 94.0;
+      case Layout.cozy:
+        return 126.0;
+    }
+  }
+
+  static double _getMaxTabWidthFromLayout(Layout layout) {
+    switch (layout) {
+      case Layout.compact:
+        return 128.0;
+      case Layout.normal:
+        return 192.0;
+      case Layout.cozy:
+        return 256.0;
+    }
+  }
+
+  static BoxDecoration _getFocusedTabBoxDecoration(
+      ThemeGeneratorParameters parameters) {
+    switch (parameters.designLanguage) {
+      case DesignLanguage.skeuomorphic:
+        return BoxDecoration(
+          color: parameters.filteredBackgroundColor,
+          border: const GradientBoxBorder(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromRGBO(255, 255, 255, 0.2),
+                Colors.transparent,
+                Colors.transparent,
+              ],
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+            ),
+            width: 1.0,
+          ),
+          borderRadius: const BorderRadius.only(
+            topLeft: Radius.circular(3.0),
+            topRight: Radius.circular(3.0),
+          ),
+          boxShadow: const [
+            BoxShadow(
+              blurRadius: 16.0,
+              color: Color.fromRGBO(0, 0, 0, 0.5),
+            )
+          ],
+        );
+
+      case DesignLanguage.flat:
+        return BoxDecoration(
+          color: parameters.filteredBackgroundColor,
+          borderRadius: const BorderRadius.only(
+            topLeft: Radius.circular(3.0),
+            topRight: Radius.circular(3.0),
+          ),
+        );
+    }
+  }
+
+  static BoxDecoration _getTabSeparatorBoxDecorationFromBrightness(
+      Brightness brightness) {
+    final color = brightness == Brightness.dark ? Colors.white : Colors.black;
+
+    return BoxDecoration(
+      gradient: LinearGradient(
+        begin: Alignment.topCenter,
+        end: Alignment.bottomCenter,
+        colors: [
+          color.withOpacity(0.0),
+          color.withOpacity(0.1),
+          color.withOpacity(0.0),
+        ],
+      ),
+    );
+  }
+
+  static BoxDecoration _getTabLightReflectionBoxDecorationFromDesignLanguage(
+      DesignLanguage designLanguage) {
+    switch (designLanguage) {
+      case DesignLanguage.skeuomorphic:
+        return const BoxDecoration(
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(3.0),
+            topRight: Radius.circular(3.0),
+          ),
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+              Color.fromRGBO(255, 255, 255, 0.3),
+              Color.fromRGBO(255, 255, 255, 0.12),
+              Color.fromRGBO(255, 255, 255, 0.1),
+              Color.fromRGBO(255, 255, 255, 0.05),
+              Color.fromRGBO(255, 255, 255, 0.02),
+              Color.fromRGBO(255, 255, 255, 0.0),
+            ],
+          ),
+        );
+
+      case DesignLanguage.flat:
+        return const BoxDecoration();
+    }
+  }
+
+  static TextStyle _getTabTextStyle(ThemeGeneratorParameters parameters) {
+    return TextStyle(
+      color: parameters.brightness == Brightness.dark
+          ? Colors.white
+          : Colors.black,
+      fontSize: parameters.layout == Layout.compact ? 10.0 : 12.0,
+      fontWeight: FontWeight.w600,
+    );
+  }
+
+  static Duration _getUnfocusedTabOpacityChangeDurationFromAnimationSpeed(
+      AnimationSpeed animationSpeed) {
+    switch (animationSpeed) {
+      case AnimationSpeed.instant:
+        return const Duration();
+      case AnimationSpeed.quick:
+        return const Duration(milliseconds: 100);
+      case AnimationSpeed.normal:
+        return const Duration(milliseconds: 150);
+      case AnimationSpeed.slow:
+        return const Duration(milliseconds: 200);
+    }
+  }
+
+  static EdgeInsets _getTabContentPaddingFromLayout(Layout layout) {
+    switch (layout) {
+      case Layout.compact:
+        return const EdgeInsets.all(4.0);
+      case Layout.normal:
+        return const EdgeInsets.all(8.0);
+      case Layout.cozy:
+        return const EdgeInsets.all(8.0);
+    }
+  }
+
+  static Duration _getTabOpenAnimationDurationFromAnimationSpeed(
+      AnimationSpeed animationSpeed) {
+    switch (animationSpeed) {
+      case AnimationSpeed.instant:
+        return const Duration();
+      case AnimationSpeed.quick:
+        return const Duration(milliseconds: 100);
+      case AnimationSpeed.normal:
+        return const Duration(milliseconds: 150);
+      case AnimationSpeed.slow:
+        return const Duration(milliseconds: 400);
+    }
   }
 }
