0e4d2887fa497ed0c8c3043326d8ce9ac2209104
Adrian Samoticha <adrian@samoticha.de>
2023-01-25 19:20:40 +0100

    implement `fromThemeGeneratorParameters` for text field theme

--- lib/config/theme_config/text_field_theme.dart
+++ lib/config/theme_config/text_field_theme.dart
@@ -1,5 +1,6 @@
 import 'package:equatable/equatable.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_color/flutter_color.dart';
 
 import 'theme_generator/theme_generator_parameters.dart';
 
@@ -75,6 +76,72 @@ class ManualWidgetTesterTextFieldTheme extends Equatable {
 
   static ManualWidgetTesterTextFieldTheme fromThemeGeneratorParameters(
       ThemeGeneratorParameters parameters) {
-    return const ManualWidgetTesterTextFieldTheme();
+    return ManualWidgetTesterTextFieldTheme(
+      textFieldShadow: _getShadowFromDesignLanguage(parameters.designLanguage),
+      textFieldColor: _getColor(parameters),
+      textFieldBorderColor:
+          _getDefaultForegroundColorFromBrightness(parameters.brightness)
+              .withOpacity(0.05),
+      textFieldTextStyle: _getDefaultTextStyle(parameters),
+      textFieldSuffixStyle: _getDefaultTextStyle(parameters),
+      textFieldCursorColor: _getCursorColor(parameters),
+      textFieldSelectionColor: _getCursorColor(parameters).withOpacity(0.67),
+      textFieldContentPadding: _getContentPaddingFromLayout(parameters.layout),
+    );
+  }
+
+  static List<BoxShadow> _getShadowFromDesignLanguage(
+      DesignLanguage designLanguage) {
+    switch (designLanguage) {
+      case DesignLanguage.skeuomorphic:
+        return const [
+          BoxShadow(
+            blurRadius: 2.0,
+            color: Color.fromRGBO(0, 0, 0, 0.2),
+            offset: Offset(0.0, 1.0),
+          ),
+        ];
+      case DesignLanguage.flat:
+        return const [];
+    }
+  }
+
+  static Color _getColor(ThemeGeneratorParameters parameters) {
+    if (parameters.brightness == Brightness.dark) {
+      return parameters.filteredBackgroundColor.lighter(10);
+    }
+
+    return parameters.filteredBackgroundColor.lighter(8);
+  }
+
+  static Color _getDefaultForegroundColorFromBrightness(Brightness brightness) {
+    return brightness == Brightness.dark ? Colors.white : Colors.black;
+  }
+
+  static TextStyle _getDefaultTextStyle(ThemeGeneratorParameters parameters) {
+    return TextStyle(
+      color: _getDefaultForegroundColorFromBrightness(parameters.brightness),
+      fontSize: 13.0,
+    );
+  }
+
+  static Color _getCursorColor(ThemeGeneratorParameters parameters) {
+    final baseColor = parameters.primaryColor;
+    if (parameters.brightness == Brightness.dark) {
+      return baseColor.lighter(10);
+    } else {
+      return baseColor.darker(10);
+    }
+  }
+
+  static EdgeInsets _getContentPaddingFromLayout(Layout layout) {
+    switch (layout) {
+      case Layout.compact:
+        return const EdgeInsets.all(4.0);
+      case Layout.normal:
+        return const EdgeInsets.all(8.0);
+      case Layout.cozy:
+        return const EdgeInsets.all(8.0);
+    }
   }
 }
