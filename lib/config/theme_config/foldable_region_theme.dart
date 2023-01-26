2c4e59347dbd485e561f79a06211fd2702e75540
Adrian Samoticha <adrian@samoticha.de>
2023-01-26 13:45:38 +0100

    implement `fromThemeGeneratorParameters` for foldable region theme

--- lib/config/theme_config/foldable_region_theme.dart
+++ lib/config/theme_config/foldable_region_theme.dart
@@ -1,5 +1,6 @@
 import 'package:equatable/equatable.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_color/flutter_color.dart';
 
 import 'theme_generator/theme_generator_parameters.dart';
 
@@ -68,6 +69,96 @@ class FoldableRegionTheme extends Equatable {
 
   static FoldableRegionTheme fromThemeGeneratorParameters(
       ThemeGeneratorParameters parameters) {
-    return const FoldableRegionTheme();
+    return FoldableRegionTheme(
+      animationDuration:
+          _getAnimationDurationFromAnimationSpeed(parameters.animationSpeed),
+      headerHeight: _getHeaderHeightFromLayout(parameters.layout),
+      headerColor: _getHeaderColor(parameters),
+      chevronIconColor:
+          _getForegroundColorFromBrightness(parameters.brightness),
+      chevronIconSize: _getChevronIconSizeFromLayout(parameters.layout),
+      headingStyle: _getHeadingStyle(parameters),
+      indentationAmount: _getIndentationAmountFromLayout(parameters.layout),
+    );
+  }
+
+  static Duration _getAnimationDurationFromAnimationSpeed(
+      AnimationSpeed animationSpeed) {
+    switch (animationSpeed) {
+      case AnimationSpeed.instant:
+        return const Duration();
+      case AnimationSpeed.quick:
+        return const Duration(milliseconds: 150);
+      case AnimationSpeed.normal:
+        return const Duration(milliseconds: 300);
+      case AnimationSpeed.slow:
+        return const Duration(milliseconds: 600);
+    }
+  }
+
+  static double _getHeaderHeightFromLayout(Layout layout) {
+    switch (layout) {
+      case Layout.compact:
+        return 16.0;
+      case Layout.normal:
+        return 24.0;
+      case Layout.cozy:
+        return 32.0;
+    }
+  }
+
+  static Color _getHeaderColor(ThemeGeneratorParameters parameters) {
+    if (parameters.brightness == Brightness.dark) {
+      return parameters.filteredBackgroundColor.lighter(16);
+    }
+
+    return parameters.filteredBackgroundColor.darker(16);
+  }
+
+  static Color _getForegroundColorFromBrightness(Brightness brightness) {
+    return brightness == Brightness.dark
+        ? Colors.white
+        : const Color.fromRGBO(0, 0, 0, 0.7);
+  }
+
+  static double _getChevronIconSizeFromLayout(Layout layout) {
+    switch (layout) {
+      case Layout.compact:
+        return 16.0;
+      case Layout.normal:
+        return 18.0;
+      case Layout.cozy:
+        return 20.0;
+    }
+  }
+
+  static TextStyle _getHeadingStyle(ThemeGeneratorParameters parameters) {
+    return TextStyle(
+      color: _getForegroundColorFromBrightness(parameters.brightness),
+      fontSize: _getHeadingSizeFromLayout(parameters.layout),
+      fontWeight: FontWeight.w700,
+    );
+  }
+
+  static double _getHeadingSizeFromLayout(Layout layout) {
+    switch (layout) {
+      case Layout.compact:
+        return 10.0;
+      case Layout.normal:
+        return 12.0;
+      case Layout.cozy:
+        return 14.0;
+    }
+  }
+
+  static double _getIndentationAmountFromLayout(Layout layout) {
+    switch (layout) {
+      case Layout.compact:
+        return 10.0;
+      case Layout.normal:
+        return 12.0;
+      case Layout.cozy:
+        return 14.0;
+    }
   }
 }
