db501cad5ba8b4ce9afb61490e23fc0283c8c3e4
Adrian Samoticha <adrian@samoticha.de>
2023-03-25 21:41:12 +0100

    implement `fromThemeGeneratorParameters` for widget test session area theme

--- lib/config/theme_config/widget_test_session_area_theme.dart
+++ lib/config/theme_config/widget_test_session_area_theme.dart
@@ -27,6 +27,38 @@ class WidgetTestSessionAreaTheme extends Equatable {
   static WidgetTestSessionAreaTheme fromThemeGeneratorParameters(
     ThemeGeneratorParameters parameters,
   ) {
-    return const WidgetTestSessionAreaTheme();
+    return WidgetTestSessionAreaTheme(
+      dottedLineColor: _getMainColorFromBrightness(parameters.brightness),
+      widgetSizeTextStyle: _getWidgetSizeTextStyle(parameters),
+    );
+  }
+
+  static Color _getMainColorFromBrightness(Brightness brightness) {
+    switch (brightness) {
+      case Brightness.dark:
+        return const Color.fromRGBO(255, 255, 255, 0.5);
+      case Brightness.light:
+        return const Color.fromRGBO(0, 0, 0, 0.5);
+    }
+  }
+
+  static double _getFontSizeFromLayout(Layout layout) {
+    switch (layout) {
+      case Layout.compact:
+        return 8.0;
+      case Layout.normal:
+        return 10.0;
+      case Layout.cozy:
+        return 12.0;
+    }
+  }
+
+  static TextStyle _getWidgetSizeTextStyle(
+    ThemeGeneratorParameters parameters,
+  ) {
+    return TextStyle(
+      color: _getMainColorFromBrightness(parameters.brightness),
+      fontSize: _getFontSizeFromLayout(parameters.layout),
+    );
   }
 }
