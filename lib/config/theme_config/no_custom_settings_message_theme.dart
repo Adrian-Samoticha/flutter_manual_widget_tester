6c35bb94900098d2b3d37f38c127e4e03af2bf16
Adrian Samoticha <adrian@samoticha.de>
2023-03-26 11:31:33 +0200

    implement `fromThemeGeneratorParameters` for “no custom settings” message theme

--- lib/config/theme_config/no_custom_settings_message_theme.dart
+++ lib/config/theme_config/no_custom_settings_message_theme.dart
@@ -28,6 +28,36 @@ class NoCustomSettingsMessageTheme extends Equatable {
   static NoCustomSettingsMessageTheme fromThemeGeneratorParameters(
     ThemeGeneratorParameters parameters,
   ) {
-    return const NoCustomSettingsMessageTheme();
+    return NoCustomSettingsMessageTheme(
+      padding: _getPadding(parameters),
+      textStyle: _getTextStyle(parameters),
+    );
+  }
+
+  static EdgeInsets _getPadding(ThemeGeneratorParameters parameters) {
+    final layout = parameters.layout;
+
+    switch (layout) {
+      case Layout.compact:
+        return const EdgeInsets.all(6.0);
+      case Layout.normal:
+        return const EdgeInsets.all(8.0);
+      case Layout.cozy:
+        return const EdgeInsets.all(12.0);
+    }
+  }
+
+  static TextStyle _getTextStyle(ThemeGeneratorParameters parameters) {
+    final brightness = parameters.brightness;
+
+    if (brightness == Brightness.light) {
+      return const TextStyle(
+        color: Color.fromRGBO(0, 0, 0, 0.75),
+      );
+    }
+
+    return const TextStyle(
+      color: Color.fromRGBO(255, 255, 255, 0.75),
+    );
   }
 }
